0fad2abddff9eaeaeb884e76459829e1d3b88509
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-30 23:50:47 -0700

    Working on the flashcard slider

--- refugio/src/app/emergency-kit/flashcards/page.tsx
+++ refugio/src/app/emergency-kit/flashcards/page.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <>
+        <section className="slider-container">
+            <div className="slider-images">
+                <div className="slider-img">
+                    <img src="" alt="" />
+                    <h1>Mike</h1>
+                    <div className="details">
+                        <h2>Mike</h2>
+                        <p></p>
+                    </div>
+                </div>    
+            </div>
+        </section>
+    </>
+)
 }
 
 export default page
